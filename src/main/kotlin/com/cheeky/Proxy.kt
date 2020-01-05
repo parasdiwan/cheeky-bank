79ae4ff4d4d80457004d87f9cb69775b8c530bc2
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-05 04:14:22 +0100

    Indent code

--- src/main/kotlin/com/cheeky/Proxy.kt
+++ src/main/kotlin/com/cheeky/Proxy.kt
@@ -3,6 +3,10 @@ package com.cheeky
 import io.javalin.Javalin
 
 fun main() {
-    val app = Javalin.create().start(8000)
-    app.get("/ping") { handler -> handler.result("pong") }
+    val app = Javalin
+        .create()
+        .start(8000)
+
+    app.get("/ping")
+        { handler -> handler.result("pong") }
 }
