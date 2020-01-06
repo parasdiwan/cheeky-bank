61ebe2a3777d23c721367ae4027e0063dc8c0310
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-06 19:16:51 +0100

    Add transaction model

---
+++ src/main/kotlin/com/cheeky/Transaction.kt
@@ -0,0 +1,15 @@
+package com.cheeky
+
+import java.util.Date
+
+class Transaction (
+    id: String,
+    creationTime: Date,
+    updateTime: Date,
+    sourceAccountId: String,
+    destinationAccountId: String,
+    status: String,
+    userId: String
+) {
+
+}
