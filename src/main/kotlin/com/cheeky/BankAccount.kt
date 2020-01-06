7af53e391d8b96cf8463e5b71f316e2f9058b183
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-06 19:17:00 +0100

    Add Bank account model

---
+++ src/main/kotlin/com/cheeky/BankAccount.kt
@@ -0,0 +1,14 @@
+package com.cheeky
+
+import java.util.Date
+
+class BankAccount (
+    id: String,
+    creationTime: Date,
+    updateTime: Date,
+    userId: String,
+    balance: Double,
+    currency: String
+){
+
+}
