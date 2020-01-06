2d97218fc0d747799fff986202c00de8b7f271bf
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-07 00:48:04 +0100

    Initialize singleton repository

--- src/main/kotlin/com/cheeky/BankAccountRepository.kt
+++ src/main/kotlin/com/cheeky/BankAccountRepository.kt
@@ -1,4 +1,15 @@
 package com.cheeky
 
 class BankAccountRepository: CheekyMemoryRepository<String, BankAccount>() {
+
+    companion object {
+        lateinit var INSTANCE: BankAccountRepository
+
+        fun getInstance(): BankAccountRepository {
+            if (INSTANCE == null) {
+                INSTANCE = BankAccountRepository()
+            }
+            return INSTANCE
+        }
+    }
 }
--- src/main/kotlin/com/cheeky/TransactionRepository.kt
+++ src/main/kotlin/com/cheeky/TransactionRepository.kt
@@ -1,4 +1,15 @@
 package com.cheeky
 
 class TransactionRepository: CheekyMemoryRepository<String, Transaction>() {
+
+    companion object {
+        lateinit var INSTANCE: TransactionRepository
+
+        fun getInstance(): TransactionRepository {
+            if (INSTANCE == null) {
+                INSTANCE = TransactionRepository()
+            }
+            return INSTANCE
+        }
+    }
 }
