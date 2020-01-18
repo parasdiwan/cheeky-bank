f194dfbb0a03c4609ae4092f837f0403dc256a09
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-19 00:47:48 +0100

    Add inject dependency

--- src/main/kotlin/com/cheeky/Proxy.kt
+++ src/main/kotlin/com/cheeky/Proxy.kt
@@ -1,8 +1,11 @@
 package com.cheeky
 
+import com.cheeky.di.DependencyManager
 import io.javalin.Javalin
 
 fun main() {
+
+    DependencyManager("com.cheeky.core").initialize()
     val app = Javalin
         .create()
         .start(8000)
---
+++ src/main/kotlin/com/cheeky/di/Inject.kt
@@ -0,0 +1,6 @@
+package com.cheeky.di
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CONSTRUCTOR)
+annotation class Inject {
+}
