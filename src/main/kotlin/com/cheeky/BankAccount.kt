32747cf63de1dc3a3457dbfeaef41e1534fcc6d1
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-18 18:44:22 +0100

    Add basic logic functions for BankAccount

--- src/main/kotlin/com/cheeky/BankAccount.kt
+++ src/main/kotlin/com/cheeky/BankAccount.kt
@@ -10,5 +10,15 @@ class BankAccount (
     var balance: Double,
     val currency: String
 ){
+    fun isAmountDeductible(amount: Double): Boolean {
+        return balance >= amount
+    }
 
+    fun deductAmount(amount: Double) {
+        balance = balance - amount
+    }
+
+    fun addAmount(amount: Double) {
+        balance = balance + amount
+    }
 }
