c9188d280073601ee6176f64427ef84465b33f8b
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-18 18:46:33 +0100

    Add basic transfer logic, no corner cases

--- src/main/kotlin/com/cheeky/TransactionService.kt
+++ src/main/kotlin/com/cheeky/TransactionService.kt
@@ -1,7 +1,51 @@
 package com.cheeky
 
+import java.lang.IllegalArgumentException
+import java.util.UUID
+import java.util.Date
+
 class TransactionService private constructor() {
 
-    fun transferMoney(sourceAccount: String, destinationAccount: String, amount: Double) {
+    val transactions: TransactionRepository = TransactionRepository.getInstance()
+    val accounts: BankAccountRepository = BankAccountRepository.getInstance()
+
+    fun transferMoney(userId: String, sourceAccountId: String, destinationAccountId: String, amount: Double) {
+
+        val sourceAccount: BankAccount = accounts.findById(sourceAccountId)
+            ?: throw IllegalArgumentException("Account not found")
+        val destinationAccount: BankAccount = accounts.findById(destinationAccountId)
+            ?: throw IllegalArgumentException("Destination account not found")
+
+        validateUserTransaction(sourceAccount, userId, amount)
+
+        val transactionId = UUID.randomUUID().toString()
+        val creationTime = Date()
+
+        val transaction = Transaction(
+            transactionId,
+            creationTime,
+            creationTime,
+            sourceAccountId,
+            destinationAccountId,
+            "Initiated",
+            userId
+            )
+
+        transactions.save(transactionId, transaction)
+
+        sourceAccount.deductAmount(amount)
+        destinationAccount.addAmount(amount)
+
+        accounts.save(sourceAccountId, sourceAccount)
+        accounts.save(destinationAccountId, destinationAccount)
+    }
+
+    private fun validateUserTransaction(sourceAccount: BankAccount, userId: String, amount: Double) {
+        if (userId != sourceAccount.userId) {
+            throw IllegalArgumentException("User not authorized to perform transfer")
+        }
+        if (sourceAccount.isAmountDeductible(amount)) {
+            throw IllegalArgumentException("Not enough balance in the source account")
+        }
     }
 }
