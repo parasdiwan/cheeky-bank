f96e08a0bc56a0f9e1069d803364d92b4ced59cd
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-20 03:49:33 +0100

    Use concurrent hashMap for storing entities

--- src/main/kotlin/com/cheeky/CheekyMemoryRepository.kt
+++ src/main/kotlin/com/cheeky/CheekyMemoryRepository.kt
@@ -3,17 +3,18 @@ package com.cheeky
 import com.cheeky.core.CheekyEntity
 import java.util.Date
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 
 abstract class CheekyMemoryRepository<T: CheekyEntity> {
 
-    private val dataStore = hashMapOf<String, T>();
+    private val dataStore = ConcurrentHashMap<String, T>();
 
     fun save(entity: T) {
         if (entity.id == null) {
             entity.id = UUID.randomUUID().toString()
         }
         entity.updateTime = Date()
-        dataStore.put(entity.id, entity)
+        dataStore[entity.id] = entity
     }
 
     fun findById(id: String): T? {
