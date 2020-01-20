1cd041326db8d2a19678ea80861eaef178bcdbca
Paras Diwan <paras.diwan@glovoapp.com>
2020-01-20 07:22:17 +0100

    Drop save endpoint from repo

--- src/main/kotlin/com/cheeky/CheekyMemoryRepository.kt
+++ src/main/kotlin/com/cheeky/CheekyMemoryRepository.kt
@@ -1,8 +1,6 @@
 package com.cheeky
 
 import com.cheeky.core.CheekyEntity
-import java.lang.IllegalStateException
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 abstract class CheekyMemoryRepository<T: CheekyEntity> (
@@ -11,29 +9,7 @@ abstract class CheekyMemoryRepository<T: CheekyEntity> (
 
     private val dataStore = ConcurrentHashMap<String, T>();
 
-    fun save(entity: T): String {
-        val key = entity.id
-        locks.lock(key)
-
-        validateVersioning(key, entity.versionNumber)
-        val entityToSave = entity.copy(Date(), UUID.randomUUID().toString()) as T
-        dataStore[key] = entityToSave
-
-        locks.unlock(key)
-        return entity.id
-    }
-
-    private fun validateVersioning(key: String, entityVersionNumber: String) {
-        if (dataStore.containsKey(key)
-            && dataStore[key]!!.versionNumber != entityVersionNumber
-        ) {
-            throw IllegalStateException("Entity update failed: Version mismatch")
-        }
-    }
-
     fun findById(id: String): T? {
-        return dataStore[id]?.copy() as T?
+        return dataStore[id]
     }
-
-
 }
